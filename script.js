b369e9da4b50ce27883ac7ee9a5ebf7f8114e251
AnuragKush13 <anurag.kush13@gmail.com>
2024-01-24 23:51:50 +0530

    added my career section

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
         fadeOut: false,  // Disable fade-out effect
         fadeIn: true ,    // Enable fade-in effect
         easing: 'easeInOutExpo',
+        
         loop: false
     };
 
